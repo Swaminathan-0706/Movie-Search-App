c16089cdc404cafe8957ce4222e7aeb8237ed5f2
Swaminathan-0706 <sayeeiyer@gmail.com>
2020-06-11 21:19:40 +0530

    Final commit

--- src/Component/SearchMovies.js
+++ src/Component/SearchMovies.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import './SearchMovies.css'
+import MovieCard from './MovieCard';
 
 function SearchMovies() {
 
@@ -11,9 +12,11 @@ function SearchMovies() {
         const URL=`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`;
         try{
             const res=await fetch(URL);
-        const data=await res.json();
-        console.log(data.results);
-        setMovies(data.results);
+            const data=await res.json();
+            setMovies(data.results);
+            if(data.results.length===0){
+                alert("No movies based on the search,Please Try with other names.");
+            }
 
         }
         catch(err){
@@ -25,26 +28,13 @@ function SearchMovies() {
         <>
             <form onSubmit={searchMovies} className="form">
                 <label htmlFor="query"className="label">Movie Name</label>
-                <input className="input" type="text" name="query" placeholder="Type Movie name you wanted to search..." value={query} onChange={(e)=>setQuery(e.target.value)}/>
+                <input className="input" type="text" name="query" placeholder="Type Movie name you wanted to search..."  onChange={(e)=>setQuery(e.target.value)}/>
                 <button className="button" type="submit">Search</button>
             </form>
-            <div className="card-list">
-                {movies.filter(movie=>movie.poster_path).map(movie=>(
-                <div className="card" key={movie.id}>
-                        <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + "poster"}></img>
-                    <div className="card--content">
-                        <h3 className="card--title">{movie.title}</h3>
-                            <p>
-                            <small>RELEASE DATE: {movie.release_date}</small>
-                            </p>
-                                <p>
-                                <small>RATING: {movie.vote_average}</small>
-                                </p>
-                                <p className="card--desc">{movie.overview}</p>
-                    </div>
-                </div>
-                ))}
-            </div>
+            {
+                <MovieCard list={movies}/> 
+            }
+              
         </>
     )
 }
